594d83f299613a9681c6b8ae224d081f9abdb7bc
Bakr Hawsawi <122125253+Bakr40@users.noreply.github.com>
2023-05-22 15:56:02 +0300

    finsh

---
+++ AppsViews/AppView/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpRequest,HttpResponse
+# Create your views here.
+
+def home_page(request:HttpRequest):
+    return (HttpResponse("Hello World, This is my new HOME !"))
+
+def about(request:HttpRequest):
+    return(HttpResponse("A simple paragraph about the website"))
