d9d413c2cd146ca10e9059e8c48ea4c40abf55b3
Yousef M <yousefmalsaadan@gmail.com>
2023-05-23 10:27:00 +0300

    update solution

--- FirstDjango/main_app/views.py
+++ FirstDjango/main_app/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 
 
-def home_page(request):
+def home_page(request: HttpRequest):
     return HttpResponse("Hello World, This is my new HOME !")
 
 
-def about(request):
+def about(request: HttpRequest):
     return HttpResponse("This websit about the Django fremwork lab.")
 
 
-def home(request):
+def home(request: HttpRequest):
     return HttpResponse("Home")
