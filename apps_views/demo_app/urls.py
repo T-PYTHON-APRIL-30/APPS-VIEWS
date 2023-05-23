c5a49cedb79b947fe89fee2152924e0fc9781f0c
saud <sasud000@gmail.com>
2023-05-23 10:26:24 +0300

    added about page

--- apps_views/demo_app/urls.py
+++ apps_views/demo_app/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 
-app_name= "TwiCo_app"
+app_name= "demo_app"
 urlpatterns = [
     path('', views.home_page, name='home_page'),
+    path('about/',views.about_page,name='about')
 ]
